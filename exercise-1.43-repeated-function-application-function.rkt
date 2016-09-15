cfc955a16ad4f3c1de26713df5fdfbdbea55ae0f
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2016-09-15 14:39:10 +0200

    solved exercise 1.43

--- exercise-1.43-repeated-function-application-function.rkt
+++ exercise-1.43-repeated-function-application-function.rkt
@@ -3,14 +3,22 @@
 (define MAX-BYTES (Mb-to-B 64))
 (custodian-limit-memory (current-custodian) MAX-BYTES)
 
+(define (square x)
+	(* x x))
+
 (define (compose f g)
 	(lambda (x)
 		(f (g x))))
 
-(define (square x)
-	(* x x))
+(define (repeated func repeated-application-count)
+	(define (iter result-function remaining-applications-count)
+		(display "remaining-applications-count: ") (display remaining-applications-count) (newline)
+		(if
+			(= remaining-applications-count 1)
+			result-function
+			(iter
+				(compose func result-function)
+				(- remaining-applications-count 1))))
+	(iter func repeated-application-count))
 
-(define (inc x)
-	(+ x 1))
-
-((compose square inc) 6)
+((repeated square 2) 5)
