1156489bb78f82b7d7d23dc4bbed9e0a114fd0cf
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2016-10-30 15:18:06 +0100

    solved exercise 2.60

--- exercise-2.60-duplicate-set.rkt
+++ exercise-2.60-duplicate-set.rkt
@@ -7,3 +7,43 @@
 (define nil '())
 (custodian-limit-memory (current-custodian) MAX-BYTES)
 
+(provide (all-defined-out))
+
+(define (element-of-duplicate-set? x set)
+  (cond [(empty? set) false]
+        [(equal? x (car set)) true]
+        [else (element-of-duplicate-set? x (cdr set))]))
+
+(define (adjoin-duplicate-set x set)
+  (cons x set))
+
+(define (intersection-duplicate-set set1 set2)
+  (define (iter result s1 s2 already-swapped)
+    (display "intersection of:") (newline)
+    (display "result: ") (display result) (newline)
+    (display "s1: ") (display s1) (newline)
+    (display "s2: ") (display s2) (newline)
+
+    (cond
+      [(and (empty? s1) (empty? s2))
+        result]
+      
+      [(and (empty? s1) (not already-swapped))
+        (iter result s2 result true)]
+
+      [(and (empty? s1) already-swapped)
+        result]
+
+      [(element-of-duplicate-set? (car s1) s2)
+        (iter (append result (list (car s1)))
+              (cdr s1)
+              s2
+              already-swapped)]
+
+      [else
+        (iter result (cdr s1) s2 already-swapped)]))
+
+  (iter '() set1 set2 false))
+
+(define (union-duplicate-set set1 set2)
+  (append set1 set2))
--- exercise-2.60-test.rkt
+++ exercise-2.60-test.rkt
@@ -1,7 +1,96 @@
 #lang racket
 
 (require rackunit
-         "exercise-2.59-union-set.rkt")
+         "exercise-2.60-duplicate-set.rkt")
 
 ;;; TEST CASES
+(test-case "element-of-duplicate-set? test case"
+           (check-equal? (element-of-duplicate-set? 1 '(2 3 1))
+                         true
+                         "element-of-duplicate-set? does not work correctly.")
+           (check-equal? (element-of-duplicate-set? 4 '(2 3 1))
+                         false
+                         "element-of-duplicate-set? does not work correctly.")
+           (check-equal? (element-of-duplicate-set? '(1) '(2 3 1))
+                         false
+                         "element-of-duplicate-set? does not work correctly.")
+           (check-equal? (element-of-duplicate-set? 'a '())
+                         false
+                         "element-of-duplicate-set? does not work correctly."))
 
+(test-case "adjoin-duplicate-set test case"
+           (check-equal? (adjoin-duplicate-set 'a '(1 2 3))
+                         '(a 1 2 3)
+                         "adjoin-duplicate-set does not work correctly.")
+           (check-equal? (adjoin-duplicate-set '() '(1 2 3))
+                         '(() 1 2 3)
+                         "adjoin-duplicate-set does not work correctly.")
+           (check-equal? (adjoin-duplicate-set '(1 2) '(1 2 3))
+                         '((1 2) 1 2 3)
+                         "adjoin-duplicate-set does not work correctly.")
+           (check-equal? (adjoin-duplicate-set '(3) '(1 2 3 (3)))
+                         '((3) 1 2 3 (3))
+                         "adjoin-duplicate-set does not work correctly.")
+           (check-equal? (adjoin-duplicate-set 3 '(1 2 3 (3)))
+                         '(3 1 2 3 (3))
+                         "adjoin-duplicate-set does not work correctly.")
+           (check-equal? (adjoin-duplicate-set 3 '(1 2 (3)))
+                         '(3 1 2 (3))
+                         "adjoin-duplicate-set does not work correctly."))
+
+(test-case "intersection-duplicate-set test case"
+           (check-equal? (intersection-duplicate-set '() '(4 5 6))
+                         '()
+                         "intersection-duplicate-set does not work correctly.")
+           (check-equal? (intersection-duplicate-set '(1 2 3) '())
+                         '()
+                         "intersection-duplicate-set does not work correctly.")
+           (check-equal? (intersection-duplicate-set '(1 2 3) '(1 2 6))
+                         '(1 2 1 2)
+                         "intersection-duplicate-set does not work correctly.")
+           (check-equal? (intersection-duplicate-set '(1 2 3) '(1 5 6))
+                         '(1 1)
+                         "intersection-duplicate-set does not work correctly.")
+           (check-equal? (intersection-duplicate-set '(1 2) '(1 2 2 3))
+                         '(1 2 1 2 2)
+                         "intersection-duplicate-set does not work correctly.")
+           (check-equal? (intersection-duplicate-set '(0 1 1 2 2 3) '(2 2 3 4))
+                         '(2 2 3 2 2 3)
+                         "intersection-duplicate-set does not work correctly."))
+
+(test-case "union-duplicate-set test case"
+   (check-true
+     (let
+       [(result (union-duplicate-set '(1 2 3) '(4 5 6)))]
+       [and (element-of-duplicate-set? 1 result)
+            (element-of-duplicate-set? 2 result)
+            (element-of-duplicate-set? 3 result)
+            (element-of-duplicate-set? 4 result)
+            (element-of-duplicate-set? 5 result)
+            (element-of-duplicate-set? 6 result)])
+     "union-duplicate-set does not work correctly.")
+   (check-equal? (union-duplicate-set '(1 2 3) '(1 2 6))
+                 '(1 2 3 1 2 6)
+                 "union-duplicate-set does not work correctly.")
+   (check-true
+     (let
+       [(result (union-duplicate-set '(1 2 3) '(1 2 6)))]
+       [and (element-of-duplicate-set? 1 result)
+            (element-of-duplicate-set? 2 result)
+            (element-of-duplicate-set? 3 result)
+            (element-of-duplicate-set? 6 result)])
+     "union-duplicate-set does not work correctly.")
+   (check-true
+     (let
+       [(result (union-duplicate-set '() '(4 5 6)))]
+       [and (element-of-duplicate-set? 4 result)
+            (element-of-duplicate-set? 5 result)
+            (element-of-duplicate-set? 6 result)])
+     "union-duplicate-set does not work correctly.")
+   (check-true
+     (let
+       [(result (union-duplicate-set '(1 2 3) '()))]
+       [and (element-of-duplicate-set? 1 result)
+            (element-of-duplicate-set? 2 result)
+            (element-of-duplicate-set? 3 result)])
+     "union-duplicate-set does not work correctly."))
