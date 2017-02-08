50f938684183f37421a513e77d37714d402ba90e
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2017-02-08 15:54:31 +0100

    apparently solved 2.66 but balanced tree  is not equal to binary tree and do not know whether I am supposed to implement binary tree creation for exercise 2.66

--- exercise-2.66-binary-tree-lookup.rkt
+++ exercise-2.66-binary-tree-lookup.rkt
@@ -34,66 +34,96 @@
     [(> x (entry myset)) (element-of-set-binary-tree? x (right-branch myset))]))
 
 ;; adds an element to a binary tree
-(define (adjoin-set x myset)
-  (cond
-    ;; if we do not have any elements yet, simply make a new tree with the given x
-    [(empty? myset) (make-tree x nil nil)]
-    ;; if we find the element x in the tree, we do not need to add it again (set logic)
-    [(= x (entry myset)) myset]
-    ;; if x is smaller than the current root of subbranches
-    ;; we make a new tree with the x inserted somewhere in the left subbranch
-    ;; this might eventually lead to an empty subtree, where we can simply insert the element or
-    ;; to a root of subbranches, which is equal to the element x, in which case we do not add it at all
-    [(< x (entry myset))
-      (make-tree
-        (entry myset)
-        (adjoin-set x (left-branch myset))
-        (right-branch myset))]
-    ;; same as for the < case but for >
-    [(> x (entry myset))
-      (make-tree
-        (entry myset)
-        (left-branch myset)
-        (adjoin-set x (right-branch myset)))]))
+;(define (adjoin-set x myset)
+;  (cond
+;    ;; if we do not have any elements yet, simply make a new tree with the given x
+;    [(empty? myset) (make-tree x nil nil)]
+;    ;; if we find the element x in the tree, we do not need to add it again (set logic)
+;    [(= x (entry myset)) myset]
+;    ;; if x is smaller than the current root of subbranches
+;    ;; we make a new tree with the x inserted somewhere in the left subbranch
+;    ;; this might eventually lead to an empty subtree, where we can simply insert the element or
+;    ;; to a root of subbranches, which is equal to the element x, in which case we do not add it at all
+;    [(< x (entry myset))
+;      (make-tree
+;        (entry myset)
+;        (adjoin-set x (left-branch myset))
+;        (right-branch myset))]
+;    ;; same as for the < case but for >
+;    [(> x (entry myset))
+;      (make-tree
+;        (entry myset)
+;        (left-branch myset)
+;        (adjoin-set x (right-branch myset)))]))
 
 ;; NEW IN EXERCISE 2.66
 ;; EXAMPLE CODE
 ;; for sets kept as unordered lists
-(define (lookup given-key set-of-records)
-  (cond
-    [(empty? set-of-records) false]
-    [(equal? given-key (key (car set-of-records)))]
-    [else (lookup given-key (cdr set-of-records))]))
+;(define (lookup given-key set-of-records)
+;  (cond
+;    [(empty? set-of-records) false]
+;    [(equal? given-key (key (car set-of-records)))]
+;    [else (lookup given-key (cdr set-of-records))]))
 
-;; MY CODE
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))  ; calculating the length of a list is in O(n)
+
+(define (partial-tree elements total-size)
+  (if
+    (= total-size 0)
+    (cons nil elements)
+    (let
+      [(left-size (quotient (- total-size 1) 2))]
+      [let
+        [(left-result (partial-tree elements left-size))]
+        [let
+          [(left-tree (car left-result))
+          (non-left-elements (cdr left-result))
+          (right-size (- total-size (+ left-size 1)))]
+          [let
+            [(this-entry (car non-left-elements))
+            (right-result (partial-tree (cdr non-left-elements) right-size))]
+            [let
+              [(right-tree (car right-result))
+              (remaining-elements (cdr right-result))]
+              [cons (make-tree this-entry left-tree right-tree) remaining-elements]]]]])))
+
+
+;; NEW IN EXERCISE 2.66
 
 ;; model a record as a list of two strings and a key
 (define (make-record key str1 str2)
-  (list key str1 str2))
+  (list key (list str1 str2)))
 
 (define (key a-record)
   (first a-record))
 
+(define (record-data a-record)
+  (rest a-record))
+
 ;; procedure for finding a record given a key in a binary tree
 (define (lookup-binary-tree given-key bin-tree-set-of-records)
+  (display "LOOKING UP ") (display given-key) (newline)
+  (if (not (empty? bin-tree-set-of-records))
+      (begin (display "TREE IS ") (display bin-tree-set-of-records) (newline)
+             (display "ENTRY IS ") (display (entry bin-tree-set-of-records)) (newline)
+             (display "ENTRY KEY IS ") (display (key (entry bin-tree-set-of-records))) (newline)
+             (display "LEFT IS ") (display (left-branch bin-tree-set-of-records)) (newline)
+             (display "RIGHT IS ") (display (right-branch bin-tree-set-of-records)) (newline))
+      (begin (display "TREE WAS EMPTY") (newline)))
+
   (cond
     [(empty? bin-tree-set-of-records)
+       (display "TREE WAS EMPTY. return false") (newline)
        false]
-    [(equal? given-key (key (entry bin-tree-set-of-records)))
+    [(= given-key (key (entry bin-tree-set-of-records)))
+       (display (string-append "SEARCHING FOR " (number->string given-key) " FOUND KEY")) (newline)
        (entry bin-tree-set-of-records)]
     [(< given-key (key (entry bin-tree-set-of-records)))
+       (display (string-append "SEARCHING FOR " (number->string given-key) " given-key is smaller")) (newline)
        (lookup-binary-tree given-key (left-branch bin-tree-set-of-records))]
     [(> given-key (key (entry bin-tree-set-of-records)))
+       (display (string-append "SEARCHING FOR " (number->string given-key) " given-key is greater")) (newline)
        (lookup-binary-tree given-key (right-branch bin-tree-set-of-records))]
     [else
       (display "strange case") (newline)]))
-
-
-(display (make-tree (make-record 2 "D" "D")
-                    (make-tree (make-record 4 "E" "E")
-                               (make-record 3 "A" "A")
-                               (make-record 5 "B" "B"))
-                    (make-record 1 "C" "C")))
-
-
-
--- exercise-2.66-test.rkt
+++ exercise-2.66-test.rkt
@@ -7,43 +7,34 @@
   "binary tree records set lookup"
 
   (check-equal?
-    (lookup-binary-tree
-      6
-      (make-tree
-        (make-record 2 "D" "D")
-        (make-tree (make-record 4 "E" "E")
-          (make-record 3 "A" "A")
-          (make-record 5 "B" "B"))
-        (make-record 1 "C" "C")))
-
+    (lookup-binary-tree 1 (list->tree (list
+                                        (make-record 5 "E" "E")
+                                        (make-record 3 "C" "C")
+                                        (make-record 7 "G" "G")
+                                        (make-record 2 "B" "B")
+                                        (make-record 4 "D" "D")
+                                        (make-record 6 "F" "F"))))
     false
-
     "not working correctly")
 
   (check-equal?
-    (lookup-binary-tree
-      5
-      (make-tree
-        (make-record 2 "D" "D")
-        (make-tree (make-record 4 "E" "E")
-          (make-record 3 "A" "A")
-          (make-record 5 "B" "B"))
-        (make-record 1 "C" "C")))
-
-    (make-record 5 "B" "B")
-
+    (lookup-binary-tree 6 (list->tree (list
+                                        (make-record 5 "E" "E")
+                                        (make-record 3 "C" "C")
+                                        (make-record 7 "G" "G")
+                                        (make-record 2 "B" "B")
+                                        (make-record 4 "D" "D")
+                                        (make-record 6 "F" "F"))))
+    (make-record 6 "F" "F")
     "not working correctly")
 
   (check-equal?
-    (lookup-binary-tree
-      1
-      (make-tree
-        (make-record 2 "D" "D")
-        (make-tree (make-record 4 "E" "E")
-          (make-record 3 "A" "A")
-          (make-record 5 "B" "B"))
-        (make-record 1 "C" "C")))
-
-    (make-record 1 "C" "C")
-
+    (lookup-binary-tree 1 (list->tree (list
+                                        (make-record 5 "E" "E")
+                                        (make-record 3 "C" "C")
+                                        (make-record 7 "G" "G")
+                                        (make-record 2 "B" "B")
+                                        (make-record 4 "D" "D")
+                                        (make-record 6 "F" "F"))))
+    (make-record 2 "B" "B")
     "not working correctly"))
