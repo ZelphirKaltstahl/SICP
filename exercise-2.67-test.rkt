8888955716056b782ba7a580ccea5c44693bf4f7
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2017-02-13 22:28:55 +0100

    solved exercise 2.67

--- exercise-2.67-test.rkt
+++ exercise-2.67-test.rkt
@@ -1,14 +1,46 @@
 #lang racket
 
 (require rackunit
-         "exercise-2.67-binary-tree-lookup.rkt")
+         "exercise-2.67-huffman-trees.rkt")
 
 (test-case
   "binary tree records set lookup"
 
   (check-equal?
-    '()
-    '()
+    (let
+      ([sample-tree
+         (combine-subtrees
+           (make-leaf 'A 4)
+           (combine-subtrees
+             (make-leaf 'C 3)
+             (combine-subtrees
+               (make-leaf 'B 2)
+               (make-leaf 'D 2))))]
+       ;; message: A B D B AA CCC D A
+       [sample-message
+         (list 0
+               1 1 0
+               1 1 1
+               1 1 0
+               0
+               0
+               1 0
+               1 0
+               1 0
+               1 1 1
+               0)])
+      (decode sample-message sample-tree))
+    (list 'A
+          'B
+          'D
+          'B
+          'A
+          'A
+          'C
+          'C
+          'C
+          'D
+          'A)
     "not working correctly")
 
   (check-equal?
