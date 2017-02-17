f1e93ff869013d6f74d65e653275f5d84e65a958
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2017-02-17 01:27:03 +0100

    corrected exercise 2.69

--- exercise-2.69-test.rkt
+++ exercise-2.69-test.rkt
@@ -7,16 +7,31 @@
   "test case for successive-merge"
 
   (check-equal?
-    (successive-merge (list (cons 'A 1)
-                            (cons 'C 2)
-                            (cons 'B 3)
-                            (cons 'D 10)))
+    (successive-merge (list (make-leaf 'A 1)
+                            (make-leaf 'C 2)
+                            (make-leaf 'B 3)
+                            (make-leaf 'D 10)))
     (combine-subtrees
-      (make-leaf 'D 10)
       (combine-subtrees
         (make-leaf 'B 3)
         (combine-subtrees
-          (make-leaf 'C 2)
-          (make-leaf 'A 1))))
+         (make-leaf 'A 1)
+         (make-leaf 'C 2)))
+      (make-leaf 'D 10))
+
+    "not working correctly")
+  
+  (check-equal?
+    (successive-merge (list (make-leaf 'A 1)
+                            (make-leaf 'C 2)
+                            (make-leaf 'B 3)
+                            (make-leaf 'D 5)))
+    (combine-subtrees
+      (make-leaf 'D 5)
+      (combine-subtrees
+        (make-leaf 'B 3)
+        (combine-subtrees
+         (make-leaf 'A 1)
+         (make-leaf 'C 2))))
 
     "not working correctly"))
